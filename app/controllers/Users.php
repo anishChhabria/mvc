e63721947b99006cb77d2d9a532a39979dfccfa5
Anish Chhabria <2017.anish.chhabria@ves.ac.in>
2018-09-16 10:22:31 +0530

    logout

--- app/controllers/Users.php
+++ app/controllers/Users.php
@@ -127,7 +127,16 @@
         // Make sure errors are empty
         if(empty($data['email_err']) && empty($data['password_err'])){
           // Validated
-            
+          //check for uswes in db
+          $loggedInUser =$this->userModel->login($data['email'],$data['password']) ;
+          if($loggedInUser){
+            // create session
+            // die('success');
+            $this->createUserSession($loggedInUser);
+          }  else{
+            $data['password_err']='password incorect';
+            $this->view('users/login', $data);
+          }
           
         } else {
           // Load view with errors
@@ -146,6 +155,29 @@
 
         // Load view
         $this->view('users/login', $data);
+      }
+    }
+      // passes the user data in session variable
+    public function createUserSession($user){
+      $_SESSION['user_id']=$user->id;
+      $_SESSION['user_email']=$user->email;
+      $_SESSION['user_name']=$user->name;
+      redirect('pages/index');
+    }
+    // logout rfunction
+    public function logout(){
+      unset($_SESSION['user_id']);
+      unset($_SESSION['user_email']);
+      unset($_SESSION['user_name']);
+      session_destroy();
+      redirect('users/login');
+    }
+    //to check wether the user has logged in or not
+    public function isLoggedIn(){
+      if(isset($_SESSION['user_id'])){
+        return true;
+      } else {
+        return false;
       }
     }
   }
--- app/views/inc/navbar.php
+++ app/views/inc/navbar.php
@@ -15,12 +15,18 @@
           </li>
         </ul>
         <ul class="navbar-nav ml-auto">
+        <?php if(isset($_SESSION['user_id'])) : ?>
+          <li class="nav-item">
+              <a class="nav-link" href="<?php echo URLROOT; ?>/users/logout">Logout</a>
+            </li>
+          <?php else : ?>
           <li class="nav-item">
             <a class="nav-link" href="<?php echo URLROOT ;?>/users/register">REGISTER </a>
           </li>
           <li class="nav-item">
             <a class="nav-link" href="<?php echo URLROOT ;?>/users/login">LOGIN</a>
           </li>
+          <?php endif; ?>
         </ul>
         </div>
   </div>
